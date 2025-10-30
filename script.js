1f1f5ed49393b0efdac8380eeb1d4730ff2dc10c
aditya8927 <adityasingh18112006@gmail.com>
2025-10-30 19:52:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ student_name = prompt("Enter your name")
 student_age = Number(prompt("Enter your age"))
 student_marks = Number(prompt("Enter your marks"))
 if (student_marks >= 95) {
-    alert(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'A+'","#Awesome")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nGrade: A+\n#Awesome`)
 }
 else if (student_marks >= 90) {
-    alert(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'A'","#Great")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nGrade: A\n#Great`)
 }
 else if (student_marks >= 80) {
-    alert(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'B'","#Good")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nGrade: B\n#Good`)
 }
 else if (student_marks >= 70) {
-    alert(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'C'","#Needs more practice")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nGrade: C\n#Needs more practice`)
 }
 else if (student_marks >= 60) {
-    alert(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'D'","#2X your efforts")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nGrade: D\n#2X your efforts`)
 }
 else if (student_marks >= 50) {
-    alert(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'E'","#marks doesn't matter !!")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nGrade: E\n#marks doesn't matter !!`)
 }
 else if (student_marks >= 40) {
-    alert(student_name, ",Age:",student_age,",marks:",student_marks , ",Grade:'F'","#boost your efforts,")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nGrade: F\n#boost your efforts`)
 }
 else {
-    alert(student_name, ", Age:",student_age,",marks:",student_marks ,", FAILED","#demoted !")
+    alert(`Name: ${student_name}\nAge: ${student_age}\nMarks: ${student_marks}\nResult: FAILED\n#demoted !`)
 }
